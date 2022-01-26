8bf7be208c626a2f16e655e38dcc738146797126
medboss3 <mohhabel@gmail.com>
2022-01-26 14:16:04 +0100

    creating first widgets for admin

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Sidebar from "./components/sidebar/sidebar";
 import Topbar from "./components/topbar/Topbar";
 import "./app.css";
+import Home from "./pages/home/Home";
+
 function App() {
   return (
     <div className="App">
       <Topbar />
       <div className="container">
         <Sidebar />
-        <di className="others">otherpages</di>
+        <Home />
       </div>
     </div>
   );
---
+++ src/components/featuredInfo/FeaturedInfo.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import "./featuredInfo.css"
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+export default function FeaturedInfo() {
+  return <div className='featured'>
+      <div className="featuredItem">
+          <span className='featuredTitle'> Revenu</span>
+          <div className="featuredMoneyContainer">
+              <span className='featuredMoney'>2,415 DT</span>
+              <span className='featuredMoneyRate'>-11.4 <ArrowDownwardIcon className='feturedIcon negative'/></span>
+          </div>
+          <span className="featuredSub">Comapred to Last month</span>
+      </div>
+      <div className="featuredItem">
+          <span className='featuredTitle'> Sales</span>
+          <div className="featuredMoneyContainer">
+              <span className='featuredMoney'>3,415 DT</span>
+              <span className='featuredMoneyRate'>-11.4 <ArrowDownwardIcon className='feturedIcon negative'/></span>
+          </div>
+          <span className="featuredSub">Comapred to Last month</span>
+      </div>
+      <div className="featuredItem">
+          <span className='featuredTitle'> Costs</span>
+          <div className="featuredMoneyContainer">
+              <span className='featuredMoney'>4,415 DT</span>
+              <span className='featuredMoneyRate'>14.4 <ArrowUpwardIcon className='feturedIcon positive'/></span>
+          </div>
+          <span className="featuredSub">Comapred to Last month</span>
+      </div>
+  </div>;
+}
