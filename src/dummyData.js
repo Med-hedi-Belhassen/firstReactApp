42c4dc7d7dc2f4feea30f4f37095f21ee1827f3d
medboss3 <mohhabel@gmail.com>
2022-01-27 14:50:02 +0100

    creating the user chart  in home page and making it reusable

---
+++ src/dummyData.js
@@ -0,0 +1,46 @@
+export const userData = [
+  {
+    name: "Jan",
+    "Active user": 4000,
+  },
+  {
+    name: "Feb",
+    "Active user": 5000,
+  },
+  {
+    name: "Mar",
+    "Active user": 3000,
+  },
+  {
+    name: "Apr",
+    "Active user": 4000,
+  },
+  {
+    name: "May",
+    "Active user": 4000,
+  },
+  {
+    name: "Jun",
+    "Active user": 2000,
+  },
+  {
+    name: "Jui",
+    "Active user": 1000,
+  },
+  {
+    name: "Sept",
+    "Active user": 5000,
+  },
+  {
+    name: "Octo",
+    "Active user": 5000,
+  },
+  {
+    name: "Nov",
+    "Active user": 1000,
+  },
+  {
+    name: "Dec",
+    "Active user": 6000,
+  },
+];
