05317bcd1302dd8cfd0ce3b0b1eb520a0a2e4d7f
medboss3 <mohhabel@gmail.com>
2022-02-02 12:03:57 +0100

    creating the user information component

---
+++ src/components/WidgetSm/WidgetSm.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import './widgetSm.css'
+import VisibilityIcon from '@mui/icons-material/Visibility';
+export default function WidgetSm() {
+  return <div className='widgetSm'>
+     <span className="widgetSmTitle">new join members</span>
+      <ul className="widgetSmList">
+        <li className="widgetSmListItem">
+          <img src="https://scontent.ftun14-1.fna.fbcdn.net/v/t1.6435-9/181270176_3953464234701700_4963932868331912823_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=N7TwypBzXxUAX_dGIG-&tn=QbMEh5YNOxS6mRpQ&_nc_ht=scontent.ftun14-1.fna&oh=00_AT-dNFHcSHRa-pevuebG_bQvYA2GiSzQ-pDGc7Rp5qHP7g&oe=6214F5B3" alt="" className="widgetSmImg" />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">hedi belhassen</span>
+            <span className="widgetSmUserTitle">Software developer</span>
+          </div>
+          <button className='widgetSmButton'>
+          <VisibilityIcon className="widgetSmIcon"/> Display
+          </button>
+        </li>
+        <li className="widgetSmListItem">
+          <img src="https://scontent.ftun15-1.fna.fbcdn.net/v/t1.6435-9/47572430_10217665281532220_8033144171436965888_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=ad2b24&_nc_ohc=nCk6ORDReK8AX8losPl&_nc_ht=scontent.ftun15-1.fna&oh=00_AT9Bg9JSe1Rid2WVZLVx3G-lstex3V2meoWA1FibycWnww&oe=621F4610" alt="" className="widgetSmImg" />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">wissem Lahmer</span>
+            <span className="widgetSmUserTitle">Software developer</span>
+          </div>
+          <button className='widgetSmButton'>
+          <VisibilityIcon className="widgetSmIcon"/> Display
+          </button>
+        </li>
+        <li className="widgetSmListItem">
+          <img src="https://scontent.ftun15-1.fna.fbcdn.net/v/t31.18172-8/c342.0.1365.1365a/s851x315/16819047_1341679422562920_5958219304415670052_o.jpg?_nc_cat=110&ccb=1-5&_nc_sid=da31f3&_nc_ohc=ewkvH-sb0L0AX-JMUaf&_nc_ht=scontent.ftun15-1.fna&oh=00_AT9G022RN35uiZKgDBxGwLnJEyMPHTXQNlYWcxe-WsFcbQ&oe=62206CCB" alt="" className="widgetSmImg" />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Souhail mihoubi</span>
+            <span className="widgetSmUserTitle">Software developer</span>
+          </div>
+          <button className='widgetSmButton'>
+          <VisibilityIcon className="widgetSmIcon"/> Display
+          </button>
+        </li>
+        <li className="widgetSmListItem">
+          <img src="https://scontent.ftun14-1.fna.fbcdn.net/v/t1.6435-9/181270176_3953464234701700_4963932868331912823_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=N7TwypBzXxUAX_dGIG-&tn=QbMEh5YNOxS6mRpQ&_nc_ht=scontent.ftun14-1.fna&oh=00_AT-dNFHcSHRa-pevuebG_bQvYA2GiSzQ-pDGc7Rp5qHP7g&oe=6214F5B3" alt="" className="widgetSmImg" />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">hedi belhassen</span>
+            <span className="widgetSmUserTitle">Software developer</span>
+          </div>
+          <button className='widgetSmButton'>
+          <VisibilityIcon className="widgetSmIcon"/> Display
+          </button>
+        </li>
+        <li className="widgetSmListItem">
+          <img src="https://scontent.ftun15-1.fna.fbcdn.net/v/t1.6435-9/47572430_10217665281532220_8033144171436965888_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=ad2b24&_nc_ohc=nCk6ORDReK8AX8losPl&_nc_ht=scontent.ftun15-1.fna&oh=00_AT9Bg9JSe1Rid2WVZLVx3G-lstex3V2meoWA1FibycWnww&oe=621F4610" alt="" className="widgetSmImg" />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">wissem Lahmer</span>
+            <span className="widgetSmUserTitle">Software developer</span>
+          </div>
+          <button className='widgetSmButton'>
+          <VisibilityIcon className="widgetSmIcon"/> Display
+          </button>
+        </li>
+        <li className="widgetSmListItem">
+          <img src="https://scontent.ftun15-1.fna.fbcdn.net/v/t31.18172-8/c342.0.1365.1365a/s851x315/16819047_1341679422562920_5958219304415670052_o.jpg?_nc_cat=110&ccb=1-5&_nc_sid=da31f3&_nc_ohc=ewkvH-sb0L0AX-JMUaf&_nc_ht=scontent.ftun15-1.fna&oh=00_AT9G022RN35uiZKgDBxGwLnJEyMPHTXQNlYWcxe-WsFcbQ&oe=62206CCB" alt="" className="widgetSmImg" />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Souhail mihoubi</span>
+            <span className="widgetSmUserTitle">Software developer</span>
+          </div>
+          <button className='widgetSmButton'>
+          <VisibilityIcon className="widgetSmIcon"/> Display
+          </button>
+        </li>
+      </ul>
+     </div>;
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,9 +3,15 @@ import Chart from '../../components/chart/Chart';
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
 import "./home.css";
 import {userData} from'../../dummyData';
+import WidgetLg from '../../components/WidgetLg/WidgetLg';
+import WidgetSm from '../../components/WidgetSm/WidgetSm';
 export default function Home() {
   return <div className='home'> 
       <FeaturedInfo/>
       <Chart data={userData} title="User Analytics" grid dataK="Active user"/>
+      <div className="homeWidgets">
+        <WidgetSm/>
+        <WidgetLg/>
+      </div>
   </div>;
 }
